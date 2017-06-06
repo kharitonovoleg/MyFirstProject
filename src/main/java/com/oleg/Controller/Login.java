aa617b5270994954bb8ba41a3d4b757ba6af49d6
Kharitonov Oleg <kharitonov.oleg@gmail.com>
2017-06-06 11:41:31 +0300

    added Controller and changed project structure

--- src/main/java/com/oleg/Controller/Login.java
+++ src/main/java/com/oleg/Controller/Login.java
@@ -25,7 +25,6 @@ public class Login extends HttpServlet  {
 
     }
 
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
